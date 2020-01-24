e39a5b3a824eb2cea841f6b83e5da2f84a29e920
Chris <26607885+chrisglein@users.noreply.github.com>
2020-01-24 14:03:55 -0800

    Make RepoUrl a functional component

--- src/RepoUrl.js
+++ src/RepoUrl.js
@@ -3,34 +3,27 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {
   View,
   Switch,
   TextInput,
 } from 'react-native';
 
-class RepoUrl extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: this.props.url,
-    };
-  }
-  render() {
-    return (
-      <View style={{flexDirection: 'row'}}>
-        <TextInput
-          style={{height: 32, flexGrow: 1}}
-          value={this.state.value}
-          onChangeText={value => this.setState({value: value})}
-          onSubmitEditing={event => this.props.onUrlChanged(event.nativeEvent.text)}/>
-        <Switch
-          value={this.props.useCache}
-          onValueChange={value => this.props.onUseCacheChanged(value)}/>
-      </View>
-    )
-  }
+let RepoUrl = (props) => {
+  let [url, setUrl] = useState(props.url);
+  return (
+    <View style={{flexDirection: 'row'}}>
+      <TextInput
+        style={{height: 32, flexGrow: 1}}
+        value={url}
+        onChangeText={value => setUrl(value)}
+        onSubmitEditing={event => props.onUrlChanged(event.nativeEvent.text)}/>
+      <Switch
+        value={props.useCache}
+        onValueChange={value => props.onUseCacheChanged(value)}/>
+    </View>
+  );
 }
 
 export { RepoUrl };
