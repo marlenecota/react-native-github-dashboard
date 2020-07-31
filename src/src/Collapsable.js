f87287e26dde097cef36c88f6a8024d84a1e23dd
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2020-07-30 17:28:03 -0700

    Collapsable header styles

--- src/src/Collapsable.js
+++ src/src/Collapsable.js
@@ -9,17 +9,26 @@ import {
 const CollapsableHeader = (props) => {
   let [expanded, setExpanded] = useState(props.expanded ?? true);
 
+  let level = parseInt(props.level) ?? 1;
+  let headerStyle;
+  switch (parseInt(props.level)) {
+    case 2: headerStyle = styles.h2; break;
+    case 3: headerStyle = styles.h3; break;
+    case 4: headerStyle = styles.h4; break;
+    default: headerStyle = styles.h1; break;
+  }
+
   return (
     <>
       <TouchableWithoutFeedback 
         accessibilityRole='header'
-        aria-level="1"
+        aria-level={level}
         onPress={() => setExpanded(!expanded)}>
         <View style={styles.collapsable}>
-          <Text accessibilityRole="header" style={styles.header}>{props.header}</Text>
+          <Text accessibilityRole="header" style={headerStyle}>{props.header}</Text>
           {expanded
-            ? <Text style={styles.expandCollapseIcon}>&#xE70D;</Text>
-            : <Text style={styles.expandCollapseIcon}>&#xE70E;</Text>
+            ? <Text style={[headerStyle, styles.expandCollapseIcon]}>&#xE70D;</Text>
+            : <Text style={[headerStyle, styles.expandCollapseIcon]}>&#xE70E;</Text>
           }
         </View>
       </TouchableWithoutFeedback>
@@ -35,11 +44,19 @@ const styles = StyleSheet.create({
   },
   expandCollapseIcon: {
     fontFamily: 'Segoe MDL2 Assets',
-    fontSize: 10,
     marginLeft: 4,
   },
-  header: {
+  h1: {
     fontSize: 24,
+  },
+  h2: {
+    fontSize: 20,
+  },
+  h3: {
+    fontSize: 14,
+  },
+  h4: {
+    fontSize: 10,
   }
 });
 
