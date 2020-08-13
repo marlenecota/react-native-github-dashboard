22bb07d34b553708415bb6767d7a9dc48e8163bd
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2020-08-12 17:55:45 -0700

    Update with RNW 0.63 preview 1

---
+++ src/windows/src/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::src::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::src::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
